ee12041e83c8584dbbda35fe0a9ef0cee85d16d8
afalinalv <afalinalv@ya.ru>
2018-11-27 22:46:21 +0300

    Стартовала AdMob баннер не кажет

--- app/src/main/java/com/example/admob345_x_k/MainActivity.kt
+++ app/src/main/java/com/example/admob345_x_k/MainActivity.kt
@@ -4,12 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.google.firebase.analytics.FirebaseAnalytics
 import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdSize
 import com.google.android.gms.ads.AdView;
-
-
-
-
-
+import com.google.android.gms.ads.MobileAds
 
 
 class MainActivity : AppCompatActivity() {
@@ -30,9 +27,24 @@ class MainActivity : AppCompatActivity() {
         bundle.putString(FirebaseAnalytics.Param.CONTENT_TYPE, "image")
         mFirebaseAnalytics.logEvent(FirebaseAnalytics.Event.SELECT_CONTENT, bundle)
 
+        // Sample AdMob app ID: ca-app-pub-3940256099942544~3347511713
+        //MobileAds.initialize(this, "YOUR_ADMOB_APP_ID")
+        MobileAds.initialize(this, "ca-app-pub-3940256099942544~3347511713")
         mAdView = findViewById(R.id.adView)
         val adRequest = AdRequest.Builder().build()
         mAdView.loadAd(adRequest)
 
+        val adView = AdView(this)
+        adView.adSize = AdSize.BANNER
+        adView.adUnitId = "ca-app-pub-3940256099942544/6300978111"
+// TODO: Add adView to your view hierarchy.
+
+
+
     }
 }
+
+
+/* https://stackoverflow.com/questions/52325837/why-my-admob-rewarded-video-ad-failed-to-load-again-and-again
+
+ */
