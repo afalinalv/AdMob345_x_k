0419502ce016e6da41be976448923011924f7262
afalinalv <afalinalv@ya.ru>
2018-11-27 19:59:53 +0300

    Alalytics

--- app/src/main/java/com/example/admob345_x_k/MainActivity.kt
+++ app/src/main/java/com/example/admob345_x_k/MainActivity.kt
@@ -2,11 +2,27 @@ package com.example.admob345_x_k
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.analytics.FirebaseAnalytics
+
+
+
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mFirebaseAnalytics: FirebaseAnalytics
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // Obtain the FirebaseAnalytics instance.
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this)
+
+        val bundle = Bundle()
+        bundle.putString(FirebaseAnalytics.Param.ITEM_ID, "id3451")
+        bundle.putString(FirebaseAnalytics.Param.ITEM_NAME, "name3451")
+        bundle.putString(FirebaseAnalytics.Param.CONTENT_TYPE, "image")
+        mFirebaseAnalytics.logEvent(FirebaseAnalytics.Event.SELECT_CONTENT, bundle)
+
     }
 }
